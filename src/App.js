ab9b74fb6533b5bacb723cc280df9edfb7a85b42
thisbra <thiagosbrasil@ua.pt>
2023-03-23 19:07:29 +0000

    Removed ua aveiro dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div>Hello from PACO2</div>;
+  return (
+    <div>
+      <h1>Welcome to my React App</h1>
+      <p>This is a sample paragraph of information.</p>
+      <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 4</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
