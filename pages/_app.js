758cb4077caefe4bf12e789037f02da715d3554c
Thiago S. Brasil <thiagosbrasil@ua.pt>
2023-03-29 20:42:20 +0100

    nextjs sync

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import '@/styles/globals.css'
+import NavBar from '@/component/navbar/navbar';
 
 export default function App({ Component, pageProps }) {
   return <>
-    {/* AQUI */}
+    <NavBar />
     <Component {...pageProps} />
   </>
 }
