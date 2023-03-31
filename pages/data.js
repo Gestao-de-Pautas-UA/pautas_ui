dfb1abf4c93d414de8d3bbcae763dcf669a825f4
yanisfaquir <yanismarinafaquir@ua.pt>
2023-03-31 18:57:28 +0100

    table implementation

---
+++ pages/data.js
@@ -0,0 +1,57 @@
+const data = {
+    hasSectionTitle: true,
+    title: "Gestão de Pautas",
+    data: `
+      <table>
+
+          <thead>
+            <tr>
+              <th>Disciplina</th>
+              <th>Época de Exame</th>
+              <th>N_Pauta</th>
+              <th>Estado</th>
+              <th>Editar</th>
+              <th>Detalhes</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Introdução a Engenharia de software</td>
+                <td>Normal</td>
+                <td>123NM</td>
+                <td><Button className="circle_btn">' '</Button></td>
+                <td><Button variant="contained">Editar</Button></td>
+                <td><Button variant="contained">Detalhes</Button></td>
+                </tr>
+            <tr>
+                <td>Projecto em Informatica</td>
+                <td>Normal</td>
+                <td>123NM</td>
+                <td><Button className="circle_btn">' '</Button></td>
+                <td><Button variant="contained">Editar</Button></td>
+                <td><Button variant="contained">Detalhes</Button></td>
+            </tr>
+            <tr>
+                <td>Compiladores</td>
+                <td>Normal</td>
+                <td>123NM</td>
+                <td><Button className="circle_btn">' '</Button></td>
+                <td><Button variant="contained">Editar</Button></td>
+                <td><Button variant="contained">Detalhes</Button></td>
+            </tr>
+            
+            <tr>
+                <td>Base de dados</td>
+                <td>Normal</td>
+                <td>123NM</td>
+                <td><Button className="circle_btn">'  '</Button></td>
+                <td><Button variant="contained">Editar</Button></td>
+                <td><Button variant="contained">Detalhes</Button></td>
+            </tr>
+          </tbody>
+      </table>
+    `
+  };
+  
+  export default data;
+  
