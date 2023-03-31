84d00270db089f8c75e0ec5ecc6e6a83f1a1ba75
Thiago S. Brasil <thiagosbrasil@ua.pt>
2023-03-31 18:32:37 +0100

    button setup

--- pages/pauta.jsx
+++ pages/pauta.jsx
@@ -1,4 +1,4 @@
-import { Table, Theme, ThemeProvider } from "@uaveiro/ui";
+import { Table, Theme, ThemeProvider, Button } from "@uaveiro/ui";
 
 
 export default function Pauta() {
@@ -58,34 +58,39 @@ export default function Pauta() {
   
     return (
 
-      <div class="pauta-page-container">
-        <div className="pauta-page-title">{ pautaName.pautaNome }</div>
-        <div class="pauta-page-button-row"></div>
-        <ThemeProvider theme={Theme}>
-          <Table marginTop="0">
-            <caption>Tabela de alunos</caption>
-            <thead>
-              <tr>
-                <th>Número Mecanográfico</th>
-                <th>Nome</th>
-                <th>Regime</th>
-                <th>Código de Curso</th>
-                <th>Repetente</th>
-              </tr>
-            </thead>
-            <tbody>
-              {studentsData.map((student) => (
+      <ThemeProvider theme={Theme}>
+        <div class="pauta-page-container">
+          <div className="pauta-page-title">{ pautaName.pautaNome }</div>
+          <div class="pauta-page-button-row">
+            <Button variant="default" width="9rem" >Download para preencher</Button>
+            <div>
+              <Button variant="default" width="9.5rem">Upload de planilha da pauta</Button>
+            </div>
+          </div>
+            <Table marginTop="0">
+              <caption>Tabela de alunos</caption>
+              <thead>
                 <tr>
-                  <td>{student.nome}</td>
-                  <td>{student.nmec}</td>
-                  <td>{student.regime}</td>
-                  <td>{student.codigoCurso}</td>
-                  <td>{student.repetente ? "Sim" : "Não" }</td>
+                  <th>Número Mecanográfico</th>
+                  <th>Nome</th>
+                  <th>Regime</th>
+                  <th>Código de Curso</th>
+                  <th>Repetente</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-        </ThemeProvider>
-      </div>
+              </thead>
+              <tbody>
+                {studentsData.map((student) => (
+                  <tr>
+                    <td>{student.nome}</td>
+                    <td>{student.nmec}</td>
+                    <td>{student.regime}</td>
+                    <td>{student.codigoCurso}</td>
+                    <td>{student.repetente ? "Sim" : "Não" }</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+      </ThemeProvider>
     );
 }
