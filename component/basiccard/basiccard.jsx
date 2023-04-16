f602fca01657d4d5ee31e9e772b9aa0e574320aa
yanisfaquir <yanismarinafaquir@ua.pt>
2023-04-16 14:32:45 +0100

    Integracao backend - CardView

--- component/basiccard/basiccard.jsx
+++ component/basiccard/basiccard.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { ThemeProvider, Theme} from "@uaveiro/ui";
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -7,59 +8,58 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import CircleIcon from '@mui/icons-material/Circle';
 import { red } from '@mui/material/colors';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 
 export default function BasicCard() {
-    const subjectData = [
-        {
-            "disciplina": "Engenharia de Software",
-            "epoca_exame": "Normal",
-            "n_pauta": "123NM",
-            "estado": "Por preencher"
-         }, 
-         {
-             "disciplina": "Base de dados",
-             "epoca_exame": "Normal",
-             "n_pauta": "456NM",
-             "estado": "Preenchido"
-         }, 
-         {
-             "disciplina": "Projecto em Informática",
-             "epoca_exame": "Recurso",
-             "n_pauta": "789RE",
-             "estado": "Assinado"
-         }, 
-       {
-             "disciplina": "APSEI",
-             "epoca_exame": "Normal",
-             "n_pauta": "101NM",
-             "estado": "Por preencher"
-        }
-    ];
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://20.123.119.238/pautasBack/pautas/10309907');
+                setData(response.data);
+            } catch(error){
+                console.error(error);
+            }
+        };
+        fetchData();
+    }, []);
+
+  
+    if (!data) {
+        return <ThemeProvider theme={Theme}>
+                <div class="pautas-page-container">
+                    {/* <TableLoading /> */}
+                </div>
+              </ThemeProvider>;
+      }
 
 
-    const cards = subjectData.map((subject, index) => {
+    const cards = data.map((subject, index) => {
       let estadoIcon;
   
-      if (subject.estado === "Por preencher") {
+      if (subject.estado === "POR_PREENCHER") {
         estadoIcon = <CircleIcon sx={{ fontSize: 10.5, color: "red" }} />;
 
-      } else if (subject.estado === "Preenchido") {
-        estadoIcon = <CircleIcon color="success" sx={{ fontSize: 10.5 }}/>;
+      } else if (subject.estado === "PREENCHIDA") {
+        estadoIcon = <CircleIcon  sx={{ fontSize: 10.5, color: "orange" }}/>;
 
-      } else if (subject.estado === "Assinado") {
-        estadoIcon = <CircleIcon color="primary" sx={{ fontSize: 10.5 }}/>;
+      } else if (subject.estado === "ASSINADA") {
+        estadoIcon = <CircleIcon color="success" sx={{ fontSize: 10.5 }}/>;
       }
   
       return (
-        <Card sx={{ width: 350, marginLeft: 5, marginTop: 15}} key={index}>
+        <Card sx={{ width: 350, height: 250, marginLeft: 5, marginTop: 15}} key={index}>
           <CardContent>
             <Typography variant="h5" component="div">
-              {subject.disciplina}
+              {subject.disciplinaResponse.nome}
             </Typography>
             <Typography variant="body2">
-              <b>Época:</b> {subject.epoca_exame} <br />
-              <b>N_Pauta:</b> {subject.n_pauta} <br />
+              <b>N_Pauta:</b> {subject.disciplinaResponse.codigo} <br />
+              <b>Época:</b> {subject.tipoExame} <br />
               <b>Estado:</b> {estadoIcon} <br />
             </Typography>
           </CardContent>
@@ -93,131 +93,6 @@ export default function BasicCard() {
       </div>
     );
   }
-
-
-
-
-  //   const cards = []; 
-
-  //   subjectData.map((subject, index) => {
-  //     cards.push(
-  //       <Card key={index} sx={{ width: 350, marginLeft: 5, marginTop: 15 }}>
-  //         <CardContent>
-  //           <div>
-  //             {subjectData.map((subject, index) => (
-  //               <div key={index}>
-  //                 <Typography variant="h5" component="div">
-  //                   {subject.disciplina}
-  //                 </Typography>
-  
-  //                 <Typography variant="body2">
-  //                   <b>Época:</b> {subject.epoca_exame} <br />
-  //                   <b>N_Pauta:</b> {subject.n_pauta} <br />
-  //                   {/* <b>Estado:</b> <CircleIcon color="success" sx={{ fontSize: 10.5 }}/> <br /> */}
-  //                   {/* <b>Estado:</b> <CircleIcon color="primary" sx={{ fontSize: 10.5 }}/> <br /> */}
-  //                   <b>Estado:</b>{" "}
-  //                   <CircleIcon sx={{ fontSize: 10.5, color: "red" }} /> <br />
-  //                 </Typography>
-  //               </div>
-  //             ))}
-  //           </div>
-  //         </CardContent>
-  //       </Card>
-  //     );
-  //   });
-
-  //       return (
-    
-  //     <div>
-  //       {cards}
-  //     </div>
-    
-  //   );
-
-  // }
-
-
-
-
-    
-
-//Gera os cards com os dados separados por card mas n gera o numero certo de cards e nem as cores certas    
-//     const cards = subjectData.map((data) => {
-//       let circleColor;
-//       switch (data.estado) {
-//         case "Por preencher":
-//           circleColor = "red";
-//           break;
-//         case "Preenchido":
-//           circleColor = "success";
-//           break;
-//         case "Assinado":
-//           circleColor = "primary";
-//           break;
-//         // default:
-//         //   circleColor = "grey";
-//       }
-  
-//       return (
-//         <Card sx={{ width: 350, marginLeft: 5, marginTop: 15 }}>
-//           <CardContent>
-//             <Typography variant="h5" component="div">
-//               {data.disciplina}
-//             </Typography>
-  
-//             <Typography variant="body2">
-//               <b>Época:</b> {data.epoca_exame} <br />
-//               <b>N_Pauta:</b> {data.n_pauta} <br />
-//               <b>Estado:</b>{" "}
-//               <CircleIcon sx={{ fontSize: 10.5, color: circleColor }} /> <br />
-//             </Typography>
-//           </CardContent>
-//         </Card>
-//       );
-//     });
-
-//     return (
-    
-//       <div>
-//         {cards}
-//       </div>
-    
-//     );
-// }
-
-
-
-
-
-//ESTE NAOOO 
-// return (
-//   <Card sx={{ width: 350 , marginLeft: 5 , marginTop: 15  }}>
-//     <CardContent>
-//       <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-//         {/* Word of the Day */}
-//       </Typography>
-      
-//       <Typography variant="h5" component="div">
-//         Introdução a Engenharia de Software
-//       </Typography>
-//       <Typography sx={{ mb: 1.5 }} color="text.secondary">
-//         {/* adjective */}
-//       </Typography>
-//       <Typography variant="body2">
-//         <b>Época:</b> Normal <br />
-//         <b>N_Pauta:</b> 123NM <br />
-//         {/* <b>Estado:</b> <CircleIcon color="success" sx={{ fontSize: 10.5 }}/> <br /> */}
-//         {/* <b>Estado:</b> <CircleIcon color="primary" sx={{ fontSize: 10.5 }}/> <br /> */}
-//         <b>Estado:</b> <CircleIcon sx={{ fontSize: 10.5 , color: red[500]}}/> <br />
-//       </Typography>
-//     </CardContent>
-//     <CardActions>
-//       <Button >Editar</Button>
-//       <Button >Detalhes</Button>
-//     </CardActions>
-//   </Card>
-// );
-// }
 
 
 
