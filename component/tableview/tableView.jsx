5df9b2250aac581633a8155f9027d6be9373a00a
yanisfaquir <yanismarinafaquir@ua.pt>
2023-04-16 14:15:56 +0100

    Integracao backend - TableView

--- component/tableview/tableView.jsx
+++ component/tableview/tableView.jsx
@@ -1,11 +1,12 @@
-import { Table, Tables } from "@uaveiro/ui";
+import { Table, Tables, TableLoading} from "@uaveiro/ui";
 import { ThemeProvider, Theme} from "@uaveiro/ui";
-import data from "./data";
 import Dropdown from '../dropdown/dropdown';
 import IconButtons from '../viewButton/viewButton';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from "@mui/material";
 import CircleIcon from '@mui/icons-material/Circle';
+import axios from 'axios';
+
 
 // export default function TableView(){
 //     return <ThemeProvider theme={Theme}>
@@ -22,32 +23,56 @@ import CircleIcon from '@mui/icons-material/Circle';
 // }
 
 export default function TableView(){
-    const subjectData = [
-        {
-            "disciplina": "Engenharia de Software",
-            "epoca_exame": "Normal",
-            "n_pauta": "123NM",
-            "estado": "Por preencher"
-        }, 
-        {
-            "disciplina": "Base de dados",
-            "epoca_exame": "Normal",
-            "n_pauta": "456NM",
-            "estado": "Preenchido"
-        }, 
-        {
-            "disciplina": "Projecto em Informática",
-            "epoca_exame": "Recurso",
-            "n_pauta": "789RE",
-            "estado": "Assinado"
-        }, 
-        {
-            "disciplina": "APSEI",
-            "epoca_exame": "Normal",
-            "n_pauta": "101NM",
-            "estado": "Por preencher"
-        }
-    ];
+    // const subjectData = [
+    //     {
+    //         "disciplina": "Engenharia de Software",
+    //         "epoca_exame": "Normal",
+    //         "n_pauta": "123NM",
+    //         "estado": "Por preencher"
+    //     }, 
+    //     {
+    //         "disciplina": "Base de dados",
+    //         "epoca_exame": "Normal",
+    //         "n_pauta": "456NM",
+    //         "estado": "Preenchido"
+    //     }, 
+    //     {
+    //         "disciplina": "Projecto em Informática",
+    //         "epoca_exame": "Recurso",
+    //         "n_pauta": "789RE",
+    //         "estado": "Assinado"
+    //     }, 
+    //     {
+    //         "disciplina": "APSEI",
+    //         "epoca_exame": "Normal",
+    //         "n_pauta": "101NM",
+    //         "estado": "Por preencher"
+    //     }
+    // ];
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://20.123.119.238/pautasBack/pautas/10309907');
+                setData(response.data);
+            } catch(error){
+                console.error(error);
+            }
+        };
+        fetchData();
+    }, []);
+
+  
+    if (!data) {
+        return <ThemeProvider theme={Theme}>
+                <div class="pautas-page-container">
+                    {/* <TableLoading /> */}
+                </div>
+              </ThemeProvider>;
+      }
+
 
     return (
         <ThemeProvider theme={Theme}>
@@ -75,23 +100,24 @@ export default function TableView(){
                         <th>Estado</th>
                         <th>Editar</th>
                         <th>Detalhes</th>
-
                     </tr>
                     </thead>
                     <tbody>
-                        {subjectData.map((subject) => (
+                        {data.map((subject, index) => (
                             
                             <tr>
-                                <td>{subject.disciplina}</td>
-                                <td>{subject.n_pauta}</td>
-                                <td>{subject.epoca_exame}</td>
+                                <td>{subject.disciplinaResponse.nome}</td>
+                                <td>{subject.disciplinaResponse.codigo}</td>
+                                <td>{subject.tipoExame}</td>
                                 <td>
-                                    {subject.estado === "Por preencher" ? (
+                                    {subject.estado === "POR_PREENCHER" ? (
                                         <CircleIcon sx={{color: "red" }} />
-                                    ) : subject.estado === "Preenchido" ? (
-                                        <CircleIcon color="primary" />
+                                    ) : subject.estado === "PREENCHIDA" ? (
+                                        // <CircleIcon color="primary" />
+                                        <CircleIcon sx={{color: "orange" }} />
                                     ) : (
-                                        <CircleIcon color="success" />
+                                        // <CircleIcon color="success" />
+                                         <CircleIcon color="primary" />
                                     )}
                                 </td>
                                 <td><a href="http://localhost:3001/pauta"><Button variant="outlined" style={{ borderRadius: 1,  backgroundColor: 'white', color: 'black', borderColor: 'black' }}>Editar</Button></a></td>
