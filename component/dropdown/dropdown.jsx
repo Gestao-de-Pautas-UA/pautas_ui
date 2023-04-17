a5bb8a4f36290ad7dad101309a4d4eed240f8f7a
yanisfaquir <yanismarinafaquir@ua.pt>
2023-04-17 12:44:41 +0100

    Dropdown data

--- component/dropdown/dropdown.jsx
+++ component/dropdown/dropdown.jsx
@@ -4,6 +4,9 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import NativeSelect from '@mui/material/NativeSelect';
 
 export default function Dropdown() {
   const [year, setYear] = React.useState('');
@@ -12,21 +15,29 @@ export default function Dropdown() {
     setYear(event.target.value);
   };
 
+  const now = new Date();
+  const yearNow = now.getFullYear().toString() + ' / ' + (now.getFullYear() + 1).toString();
+  const yearNext = (now.getFullYear() + 1).toString() + ' / ' + (now.getFullYear() + 2).toString();
+  const yearThen = (now.getFullYear() + 2).toString() + ' / ' + (now.getFullYear() + 3).toString();
+
+
+  const [tfValue, setTFValue] = useState(yearNow);
+
   return (
-    <Box sx={{ width: 130 , marginLeft: 5 , marginTop: 2 }}>
+    <Box sx={{ width: 160 , marginLeft: 5 , marginTop: 2 }}>
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Ano</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={year}
-          label="yaer"
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>2020/2021</MenuItem>
-          <MenuItem value={20}>2021/2022</MenuItem>
-          <MenuItem value={30}>2022/2023</MenuItem>
-        </Select>
+        <InputLabel variant="standard" htmlFor="uncontrolled-native">Ano lectivo</InputLabel>
+        <NativeSelect
+          defaultValue={30}
+          inputProps={{
+          name: 'age',
+          id: 'uncontrolled-native',
+        }}
+  >
+          <option onClick={()=>{setTFValue(tfValue)}} value={yearNow}>{yearNow}</option>
+          <option onClick={()=>{setTFValue(tfValue)}} value={yearNext}>{yearNext}</option>
+          <option onClick={()=>{setTFValue(tfValue)}} value={yearThen}>{yearThen}</option>
+        </NativeSelect>
       </FormControl>
     </Box>
   );
