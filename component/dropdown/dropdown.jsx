b7e23db3e81f2aea1d20c83677e3f69d446eed16
yanisfaquir <yanismarinafaquir@ua.pt>
2023-04-19 16:00:07 +0100

    Dropdown Conectado api

--- component/dropdown/dropdown.jsx
+++ component/dropdown/dropdown.jsx
@@ -5,67 +5,49 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { useState, useEffect } from "react";
-import TextField from "@mui/material/TextField";
-import NativeSelect from '@mui/material/NativeSelect';
-import { ThemeProvider, Theme} from "@uaveiro/ui";
 import axios from 'axios';
 
 export default function Dropdown() {
 
+  //Chamada a api
+  const [data, setData] = useState(null);
+  useEffect(() => {
+      const fetchData = async () => {
+          try {
+              const response = await axios.get('http://20.123.119.238/pautasBack/pautas/10309907');
+              setData(response.data);
+              console.log(response.data);
+          } catch(error){
+              console.error(error);
+          }
+      };
+      fetchData();
+  }, []);
 
-  const now = new Date();
-  const yearNow = now.getFullYear().toString() + ' / ' + (now.getFullYear() + 1).toString();
-  const yearNext = (now.getFullYear() + 1).toString() + ' / ' + (now.getFullYear() + 2).toString();
-  const yearThen = (now.getFullYear() + 2).toString() + ' / ' + (now.getFullYear() + 3).toString();
+  //Retira os anos repetidos
+  const uniqueData = [];
+  data && data.forEach((choice) => {
+    if (!uniqueData.includes(choice.anoLectivo)) {
+      uniqueData.push(choice.anoLectivo);
+      console.log(uniqueData);
+    }
+    });
 
-  const [tfValue, setTFValue] = useState(yearNow);
-
-  
-
-  // const [data, setData] = useState(null);
-
-  // useEffect(() => {
-  //     const fetchData = async () => {
-  //         try {
-  //             const response = await axios.get('http://20.123.119.238/pautasBack/pautas/10309907');
-  //             setData(response.data);
-  //         } catch(error){
-  //             console.error(error);
-  //         }
-  //     };
-  //     fetchData();
-  // }, []);
-
-
-
-
-  return (
-    <Box sx={{ width: 120 , marginLeft: 5 , marginTop: 2 }}>
-      <FormControl fullWidth>
-        <InputLabel variant="standard" htmlFor="uncontrolled-native">Ano lectivo</InputLabel>
-        <NativeSelect
-          defaultValue={30}
-          inputProps={{
-          name: 'anoLectivo',
-          id: 'uncontrolled-native',
-        }}
-        >
-          <option onClick={()=>{setTFValue(tfValue)}} value={yearNow}>{yearNow}</option> 
-          <option onClick={()=>{setTFValue(tfValue)}} value={yearNext}>{yearNext}</option>
-          <option onClick={()=>{setTFValue(tfValue)}} value={yearThen}>{yearThen}</option>
-
-        {/* {data.map((subject, index) => ( 
-          <option onClick={()=>{setTFValue(tfValue)}} value={subject.anoLectivo}>{subject.anoLectivo}</option> 
-          // <option onClick={()=>{setTFValue(tfValue)}} value={yearNext}>{yearNext}</option>
-          // <option onClick={()=>{setTFValue(tfValue)}} value={yearThen}>{yearThen}</option>
-        ))} */}
-
-         
-        </NativeSelect>
-      </FormControl>
-    </Box>
+    //Dropdown que exibe os anos lectivos
+    return (
+      <Box  sx={{ width: 130 , marginLeft: 5 , marginTop: 2 }}>
+        <FormControl fullWidth>
+          <InputLabel variant="standard" htmlFor="uncontrolled-native" sx={{marginLeft: 2}}>Ano</InputLabel>
+        <Select label="Question">
+          {uniqueData.map((choice) => (
+            <MenuItem key={choice} value={choice}>
+              {choice}
+            </MenuItem>
+          ))}
+        </Select>
+        </FormControl>
+      </Box>
+    );
 
 
-
-  );
 }
