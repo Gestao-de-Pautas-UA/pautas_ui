2fdd32c5b97df16a7d4325a8339564f5d5a62c61
Rui Oliveira <rui.junior@ua.pt>
2023-05-26 17:38:34 +0100

    Deploy to google

--- next.config.js
+++ next.config.js
@@ -4,10 +4,11 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    API_URL: process.env.API_URL,
-  }
+    reactStrictMode: true,
+    env: {
+        API_URL: process.env.API_URL,
+    },
+    output: "standalone"
 }
 
 module.exports = nextConfig
